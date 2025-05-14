02492670dc0d9e01bd7f4ac818b4aad3f8b36bd7
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-13 20:38:07 -0500

    Subiendo Proyecto Base a GitHub

--- src/services/dogsApi.js
+++ src/services/dogsApi.js
@@ -1,37 +1,22 @@
+import { peticionesfetch } from "../utils/apiUtils";
+
 // src/services/dogsApi.js
 const DOG_API_KEY = import.meta.env.VITE_DOG_API_KEY || 'your-default-key';
 const DOG_API_URL = 'https://api.thedogapi.com/v1';
 
 export const getAllDogBreeds = async () => {
-  try {
-    const response = await fetch(`${DOG_API_URL}/breeds`, {
-      headers: {
-        'x-api-key': DOG_API_KEY
-      }
-    });
-    if (!response.ok) {
-      throw new Error('Error fetching dog breeds');
-    }
-    return await response.json();
-  } catch (error) {
-    console.error('Error:', error);
-    return [];
-  }
+  
+  const url = `${DOG_API_URL}/breeds`;
+  const mensajeError = 'Error fetching dog breeds';
+
+  return peticionesfetch(url, DOG_API_KEY, mensajeError);
 };
 
 export const getDogImagesByBreed = async (breedId) => {
-  try {
-    const response = await fetch(`${DOG_API_URL}/images/search?breed_ids=${breedId}&limit=10`, {
-      headers: {
-        'x-api-key': DOG_API_KEY
-      }
-    });
-    if (!response.ok) {
-      throw new Error('Error fetching dog images');
-    }
-    return await response.json();
-  } catch (error) {
-    console.error('Error:', error);
-    return [];
-  }
+ 
+
+  const url = `${DOG_API_URL}/images/search?breed_ids=${breedId}&limit=10`;
+  const mensajeError = 'Error fetching dog images';
+
+  return peticionesfetch(url,DOG_API_KEY, mensajeError);
 };
