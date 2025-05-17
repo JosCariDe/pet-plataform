1dfdcadde2aa1fdbac4d79955e6da801af74b8f9
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-17 14:26:36 -0500

    Feat: creado nuevos directorios y archivos Por ahora se manejara con un Json, despues conectará con la Api del Back User context creado para saber cual es el user activo en el momento

---
+++ src/data/users.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    id: "usr_001",
+    nombre: "María García Rodríguez",
+    mascotasFavoritas: [
+      { idMascota: 1, tipoMascota: "perro" },
+      { idMascota: "cat_breed_012", tipoMascota: "gato" },
+    ],
+    mascotasRecomendadas: [],
+  },
+  {
+    id: "usr_002",
+    nombre: "Luis Pérez",
+    mascotasFavoritas: [
+      { idMascota: "dog_breed_234", tipoMascota: "perro" },
+    ],
+    mascotasRecomendadas: [],
+  },
+  {
+    id: "usr_003",
+    nombre: "Ana Torres",
+    mascotasFavoritas: [],
+    mascotasRecomendadas: [],
+  },
+];
+
+export default users;
