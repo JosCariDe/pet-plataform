5048c2a34ef9ad16fc5a7679b462023b086fd318
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-17 19:47:38 -0500

    Feat: Se agregó btn para guardar favoritos Falta conectar a api el guardado y eliminado de favoritos

--- src/components/BreedCard/BreedCard.jsx
+++ src/components/BreedCard/BreedCard.jsx
@@ -12,13 +12,6 @@ const BreedCard = ({name, origin , description, image, id, petType}) => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4 relative flex flex-col justify-between">
                                           {/* Botón de favorito */}
-                <button
-                  onClick={() => toggleFavorite(id, petType === "gatos" ? "gato" : "perro")}
-                  className="absolute top-2 right-2 text-red-500 text-xl"
-                  title={favorito ? "Quitar de favoritos" : "Agregar a favoritos"}
-                >
-                  {favorito ? <FaHeart /> : <FaRegHeart />}
-                </button>
             {image && (
                 <img src={image} alt={name} className="w-full h-48 object-contain rounded mb-4"/>
             )}
@@ -28,13 +21,22 @@ const BreedCard = ({name, origin , description, image, id, petType}) => {
                     <p className="text-gray-600">Origen: {origin}</p>
                     <p className="mt-2">{description}</p>
                 </div>
-
-                <Link
-                    to={`/${petType}/${id}`}
-                    className="mt-auto bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 text-center block"
+                <div className="flex justify-between items-center">
+                    <Link
+                        to={`/${petType}/${id}`}
+                        className="mt-auto bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 text-center block"
                     >
-                    Ver detalles
-                </Link>
+                        Ver detalles
+                    </Link>
+                    {/* Botón de favorito */}
+                    <button
+                      onClick={() => toggleFavorite(id, petType === "gatos" ? "gato" : "perro")}
+                      className="text-red-500 text-xl"
+                      title={favorito ? "Quitar de favoritos" : "Agregar a favoritos"}
+                    >
+                      {favorito ? <FaHeart /> : <FaRegHeart />}
+                    </button>
+                </div>
             </div>
             
         </div>
