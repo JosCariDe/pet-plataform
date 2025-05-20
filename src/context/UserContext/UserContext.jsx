7cb6ff2b3cc5ed3065fadfbaf53d313ce03a7996
joseLaptop <josenarvaezcariaga@gmail.com>
2025-05-20 10:49:08 -0500

    Avanzando en el arreglo de la lista de users

--- src/context/UserContext/UserContext.jsx
+++ src/context/UserContext/UserContext.jsx
@@ -4,6 +4,7 @@ import users from "../../data/users";
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+  //Falta editar este useState Para mantener seleccion incluso cuando recarga.
   const [currentUserId, setCurrentUserId] = useState(users[0].id);
 
   const currentUser = users.find((user) => user.id === currentUserId);
